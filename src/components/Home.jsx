a3b778fe63105fe59f857228ed2b3f9439d7fd02
Gikuhi <richardgikm@gmail.com>
2024-06-07 12:31:50 +0300

    Updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -42,6 +42,34 @@ export default function Home(){
 </div>
 
     {/* Carousel */}
+    <div className="carousel w-full">
+  <div id="item1" className="carousel-item w-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
+    <div className="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+  </div> 
+  <div id="item2" className="carousel-item w-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
+  </div> 
+  <div id="item3" className="carousel-item w-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+  </div> 
+  <div id="item4" className="carousel-item w-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
+  </div>
+</div> 
+
+
+<div className="flex justify-center w-full py-2 gap-2">
+  <a href="#item1" className="btn btn-xs">1</a> 
+  <a href="#item2" className="btn btn-xs">2</a> 
+  <a href="#item3" className="btn btn-xs">3</a> 
+  <a href="#item4" className="btn btn-xs">4</a>
+</div>
+
+  {/* Carousel 2 */}
     <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
